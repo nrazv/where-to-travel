ac522b61ebbc9ece63ce6d69449fe76013bd5fad
razvan <peter00nr@outlook.com>
2024-02-27 06:49:18 +0100

    Style refactoring & tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
 import { Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
-import { Paper } from "@mui/material";
+import { Paper, ThemeProvider, createTheme } from "@mui/material";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import useAppRoutes from "./hooks/routes/useAppRoutes";
+import { indigo,} from "@mui/material/colors";
 
 const App: React.FC = () => {
   const queryClient = new QueryClient();
+  const theme = createTheme({
+    palette: {
+      primary: indigo
+    },
+  });
 
   return (
+    <ThemeProvider theme={theme}>
     <QueryClientProvider client={queryClient}>
       <Navbar />
       <Paper className="App" elevation={0}>
@@ -17,6 +24,7 @@ const App: React.FC = () => {
       </Paper>
       <ReactQueryDevtools position="bottom-right" />
     </QueryClientProvider>
+    </ThemeProvider>
   );
 };
 
--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,10 +1,11 @@
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import Link from "@mui/material/Link";
 import { NavLink } from "react-router-dom";
 import { grey } from "@mui/material/colors";
 import "./Nav.css";
 
 const Navbar = () => {
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -13,14 +14,14 @@ const Navbar = () => {
             Where To Travel
           </Typography>
           <NavLink to="signup">
-            <Link color={grey[50]} component="button" variant="body1">
+            <Link  color={grey[100]} component="button" variant="body1">
               Sign Up
             </Link>
           </NavLink>
           <NavLink to="login">
             <Link
               sx={{ padding: "1rem" }}
-              color={grey[50]}
+              color={grey[100]}
               component="button"
               variant="body1"
             >
@@ -33,7 +34,7 @@ const Navbar = () => {
           >
             <Link
               sx={{ padding: "1rem" }}
-              color={grey[50]}
+              color={grey[100]}
               component="button"
               variant="body1"
             >
